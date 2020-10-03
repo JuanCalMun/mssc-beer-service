cb0502a8962f63d56397b0ecdf4cdd6f586c5ca5
JuanCalMun <juancalmun@gmail.com>
2020-10-03 21:40:10 +0200

    #4 Created JPA Entity for Beer object

---
+++ src/main/java/guru/springframework/msscbeerservice/domain/Beer.java
@@ -0,0 +1,48 @@
+package guru.springframework.msscbeerservice.domain;
+
+import guru.springframework.msscbeerservice.model.BeerStyleEnum;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import java.util.UUID;
+
+@Data
+@Entity
+public class Beer implements Serializable {
+
+    private static final long serialVersionUID = 8396569318236549200L;
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(length = 36, columnDefinition = "varchar", updatable = false, nullable = false)
+    private UUID id;
+
+    @Version
+    private Long version;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private Timestamp createdDate;
+    @UpdateTimestamp
+    private Timestamp lastMofdifiedDate;
+
+    private String beerName;
+
+    @Enumerated(EnumType.STRING)
+    private BeerStyleEnum beerStyle;
+
+    @Column(unique = true)
+    private Long upc;
+
+    private BigDecimal price;
+
+    private Integer minOnHand;
+    private Integer quantityToBrew;
+}
