1a150329ae81298630e59c3ac522541884676756
JuanCalMun <juancalmun@gmail.com>
2020-10-11 11:34:58 +0200

    Fixed broken test caused by validations. Closes #11

--- src/test/java/guru/springframework/msscbeerservice/controller/BeerRestControllerTest.java
+++ src/test/java/guru/springframework/msscbeerservice/controller/BeerRestControllerTest.java
@@ -1,14 +1,18 @@
 package guru.springframework.msscbeerservice.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import guru.springframework.msscbeerservice.model.BeerDto;
+import guru.springframework.msscbeerservice.model.BeerStyleEnum;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.math.BigDecimal;
 import java.util.UUID;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(BeerRestController.class)
@@ -27,25 +31,34 @@ class BeerRestControllerTest {
                 .andExpect(status().isOk());
     }
 
-//TODO refactor this tests
-
-/*    @Test
+    @Test
     void createBeer() throws Exception {
-        BeerDto beerDto = BeerDto.builder().build();
+        BeerDto beerDto = BeerDto.builder()
+                .beerName("TestingBeer")
+                .beerStyle(BeerStyleEnum.PALE_ALE)
+                .price(BigDecimal.valueOf(1.2))
+                .upc(12L)
+                .build();
         String beerDtoJson = objectMapper.writeValueAsString(beerDto);
         mockMvc.perform(post("/api/v1/beer")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(beerDtoJson))
                 .andExpect(status().isCreated());
     }
+
     @Test
     void update() throws Exception {
-        BeerDto beerDto = BeerDto.builder().build();
+        BeerDto beerDto = BeerDto.builder()
+                .beerName("TestingBeer")
+                .beerStyle(BeerStyleEnum.PALE_ALE)
+                .price(BigDecimal.valueOf(1.2))
+                .upc(12L)
+                .build();
         String beerDtoJson = objectMapper.writeValueAsString(beerDto);
         mockMvc.perform(put("/api/v1/beer/" + UUID.randomUUID().toString())
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(beerDtoJson))
                 .andExpect(status().isNoContent());
 
-    }*/
+    }
 }
